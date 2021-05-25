f02bf7cd6d43cf326ace8dba1739389c8a767932
Kody Kaftousian <cyrusdagrate@gmail.com>
2021-05-25 15:36:53 -0500

    updated name

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: "Kody" });
+  res.status(200).json({ name: "Kody Kaftousian" });
 };
