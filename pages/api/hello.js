346ba22c4cb1346bd7758fbc607c8809e313562c
Kody Kaftousian <cyrusdagrate@gmail.com>
2021-05-27 15:14:47 -0500

    adding art assets, and custom css

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
 export default (req, res) => {
-  res.status(200).json({ name: "Kody Kaftousian" });
+  axios
+    .get("https://s3.amazonaws.com/br-codingexams/restaurants.json")
+    .then(async (data) => {
+      res.status(200).json({ data: await data.data });
+    })
+    .catch((error) => {
+      res.status(400).json({ data: error });
+    });
 };
